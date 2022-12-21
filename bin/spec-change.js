36fdd72d91414c3e3038a8f001a770ca38306600
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-21 14:54:41 -0500

    add CI to call the demo

--- bin/spec-change.js
+++ bin/spec-change.js
@@ -2,6 +2,9 @@
 
 const arg = require('arg')
 const debug = require('debug')('spec-change')
+const globby = require('globby')
+const path = require('path')
+const { getDependentFiles } = require('../src')
 
 const args = arg({
   '--folder': String,
@@ -10,3 +13,12 @@ const args = arg({
 })
 
 debug('arguments %o', args)
+
+const folder = path.resolve(args['--folder'])
+debug('absolute folder: %s', folder)
+const files = globby.sync(folder + '/**/*.js')
+console.log('found %d files %o', files.length, files)
+
+const deps = getDependentFiles(files, folder)
+const depsStringified = JSON.stringify(deps, null, 2)
+console.log(depsStringified + '\n')
